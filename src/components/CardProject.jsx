a02486af9d57f41dc2e1ed9e28b0bd8451fa55b8
areil <maldonadoaa.91@gmail.com>
2022-12-08 20:13:31 -0300

    img

---
+++ src/components/CardProject.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import '../styles/card-project-style.css'
+
+const CardProject = () => {
+  return (
+    <div>
+      
+      <div className="card-proj">
+
+        <div className="card-img-proj">
+          <img src='' alt="" />
+          <h3 className='card-title-proj'> Project 1 </h3>
+        </div>
+
+        <div className="card-content-proj">
+          Descripción del project
+        </div>
+
+      </div>
+
+
+    </div>
+  );
+};
+
+export default CardProject;
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import '../styles/projects-style.css'
+import CardProject from './CardProject';
 
 const Projects = () => {
   return (
     <div className='project-container'>
       <h1>Projects</h1>
+
+      <CardProject/>
     </div>
   );
 };
